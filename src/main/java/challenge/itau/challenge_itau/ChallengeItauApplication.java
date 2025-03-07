9e8f95929db984c645c5b11c8854ac379c6a7286
Nagi <nagiitoshi@gmail.com>
2025-03-07 19:23:27 -0300

    first commit

---
+++ src/main/java/challenge/itau/challenge_itau/ChallengeItauApplication.java
@@ -0,0 +1,13 @@
+package challenge.itau.challenge_itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/challenge/itau/challenge_itau/ChallengeItauApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.itau.challenge_itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
